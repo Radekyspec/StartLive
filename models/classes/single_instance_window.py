76dee56bfb3a937cef7a28d94138ed79e8d896a4
Radekyspec <93813543+Radekyspec@users.noreply.github.com>
2025-06-09 12:50:34 -0400

    [0.3.3] optimize socket call

--- models/classes/single_instance_window.py
+++ models/classes/single_instance_window.py
@@ -7,22 +7,23 @@ from constant import LOCAL_SERVER_NAME
 class SingleInstanceWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.server = QLocalServer(self)
+        self._server = QLocalServer(self)
         # 清除可能残留的 socket 文件
         QLocalServer.removeServer(LOCAL_SERVER_NAME)
 
-        if not self.server.listen(LOCAL_SERVER_NAME):
+        if not self._server.listen(LOCAL_SERVER_NAME):
             QMessageBox.critical(self, "应用初始化", "启动本地服务失败！")
         else:
-            self.server.newConnection.connect(self._handle_new_connection)
+            self._server.newConnection.connect(self._handle_new_connection)
 
     def _handle_new_connection(self):
-        """收到来自其他实例的连接请求时激活窗口"""
-        socket = self.server.nextPendingConnection()
-        if socket and socket.waitForReadyRead(1000):
-            message = socket.readAll().data().decode()
-            if message == "ACTIVATE":
-                self._bring_to_front()
+        socket = self._server.nextPendingConnection()
+        socket.readyRead.connect(lambda: self._read_and_activate(socket))
+
+    def _read_and_activate(self, socket):
+        data = socket.readAll().data().decode()
+        if data == "ACTIVATE":
+            self._bring_to_front()
         socket.close()
 
     def _bring_to_front(self):
@@ -38,10 +39,10 @@ class SingleInstanceWindow(QMainWindow):
         """尝试连接到现有实例"""
         socket = QLocalSocket()
         socket.connectToServer(LOCAL_SERVER_NAME)
-        if socket.waitForConnected(500):
+        if socket.waitForConnected(50):
             socket.write(b"ACTIVATE")
             socket.flush()
-            socket.waitForBytesWritten(1000)
+            socket.waitForBytesWritten(50)
             socket.close()
             return True
         return False
