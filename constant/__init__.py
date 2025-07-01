094c45e3fc9eaeac1bc5cd294b1236a651d66117
Radekyspec <w41k3r15347@gmail.com>
2025-07-01 00:36:46 -0400

    [0.4.1] attempt to fix mac

--- constant/__init__.py
+++ constant/__init__.py
@@ -9,34 +9,31 @@ KEYRING_COOKIES = "cookies"
 KEYRING_SETTINGS = "settings"
 KEYRING_ROOM_INFO = "roomInfo"
 LOCAL_SERVER_NAME = "StartLive|singleInstanceServer"
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 
-# Might change
-_loaded = None
-
-
-def _load_constants():
-    global _loaded
-    if _loaded is None:
-        try:
-            json_path = os.path.join(os.path.dirname(__file__), "resources",
-                                     "version.json")
-            with open(json_path, "r", encoding="utf-8") as f:
-                _loaded = loads(f.read())
-        except FileNotFoundError:
-            json_path = os.path.join(os.path.dirname(__file__), "..",
-                                     "resources", "version.json")
-            with open(json_path, "r", encoding="utf-8") as f:
-                _loaded = loads(f.read())
-    return _loaded
-
-
-_ver = _load_constants()
-APP_KEY = _ver["ak"]
-APP_SECRET = _ver["as"]
-LIVEHIME_BUILD = _ver["b"]
-LIVEHIME_VERSION = _ver["v"]
-HEADERS_WEB = _ver["hw"]
-HEADERS_APP = _ver["ha"]
-START_LIVE_AUTH_CSRF = _ver["start_ac"]
-STOP_LIVE_AUTH_CSRF = _ver["stop_ac"]
+APP_KEY = "aae92bc66f3edfab"
+APP_SECRET = "af125a0d5279fd576c1b4418a3e8276d"
+LIVEHIME_BUILD = "9240"
+LIVEHIME_VERSION = "7.16.0.9240"
+HEADERS_WEB = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "Connection": "keep-alive",
+    "Origin": "https://live.bilibili.com",
+    "Referer": "https://live.bilibili.com/",
+    "sec-ch-ua": "\"Chromium\";v=\"105\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "User-Agent": "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 pc_app/livehime build/9240"
+  }
+HEADERS_APP = {
+    "Accept-Encoding": "gzip,deflate",
+    "Connection": "keep-alive",
+    "User-Agent": "LiveHime/7.16.0.9240 os/Windows pc_app/livehime build/9240 osVer/10.0_x86_64"
+  }
+START_LIVE_AUTH_CSRF = True
+STOP_LIVE_AUTH_CSRF = False
