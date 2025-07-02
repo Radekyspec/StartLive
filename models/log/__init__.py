50b77a123fac3f87cbbf16f0a299f2d661ea53d9
Radekyspec <w41k3r15347@gmail.com>
2025-07-02 06:05:49 -0400

    [0.4.2] fix log position in macOS

--- models/log/__init__.py
+++ models/log/__init__.py
@@ -3,6 +3,7 @@ from logging import DEBUG
 from logging import Formatter, Logger, LoggerAdapter, StreamHandler
 from logging import getLogger
 from logging.handlers import TimedRotatingFileHandler
+from platform import system
 
 from constant import LOGGER_NAME
 
@@ -16,13 +17,26 @@ class ThreadClassFormatter(Formatter):
 def init_logger(name: str = LOGGER_NAME) -> Logger:
     logger = getLogger(name)
     logger.setLevel(DEBUG)
-    try:
-        base_dir = os.path.abspath(__compiled__.containing_dir)
-    except NameError:
-        base_dir = os.path.abspath(".")
-    os.makedirs(os.path.join(base_dir, "logs"), exist_ok=True)
-    log_dir = os.path.join(base_dir, "logs")
-    log_path = os.path.join(log_dir, "StartLive.log")
+    if system() == "Windows":
+        try:
+            base_dir = os.path.abspath(__compiled__.containing_dir)
+        except NameError:
+            base_dir = os.path.abspath(".")
+        os.makedirs(os.path.join(base_dir, "logs"), exist_ok=True)
+        log_dir = os.path.join(base_dir, "logs")
+        log_path = os.path.join(log_dir, "StartLive.log")
+    elif system() == "Linux":
+        base_dir = os.path.join("var", "log", "StartLive")
+        os.makedirs(base_dir, exist_ok=True)
+        log_dir = base_dir
+        log_path = os.path.join(log_dir, "StartLive.log")
+    elif system() == "Darwin":
+        base_dir = os.path.join(os.path.expanduser("~"), "Library", "Logs", "StartLive")
+        os.makedirs(base_dir, exist_ok=True)
+        log_dir = base_dir
+        log_path = os.path.join(log_dir, "StartLive.log")
+    else:
+        raise ValueError("Unsupported system")
     fh = TimedRotatingFileHandler(
         log_path, when="midnight", interval=1, backupCount=0, encoding="utf-8"
     )
