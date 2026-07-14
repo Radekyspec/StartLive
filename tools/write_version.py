0cbde48883c7a46bdf7936094dff308f3d06f70f
Radekyspec <w41k3r15347@gmail.com>
2026-07-14 18:34:25 +0800

    fix: packer version

--- tools/write_version.py
+++ tools/write_version.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 version = os.environ.get("APP_VERSION", "0.0.0-dev")
 version_file = Path(__file__).resolve().parents[
-                   1] / "src" / "constant" / "_version.py"
+                   1] / "src" / "core" / "constant" / "_version.py"
 version_file.write_text(f'__version__ = "{version}"\n', encoding="utf-8")
